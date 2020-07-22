3a1e1acb2e07e57ca7eb33e6f55dacda04af63a4
Joyson Gaurea <jng05929@gmail.com>
2020-07-22 10:29:07 +0530

    create build

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import BFS from "./BFS/BFS";
 function App() {
   return (
     <div className="App">
+      <nav>
+        <div className="nav-wrapper">
+          <a href="#!" className="brand-logo center">
+            Shortest Path Visualiser
+          </a>
+        </div>
+      </nav>
+
       <BFS></BFS>
     </div>
   );
